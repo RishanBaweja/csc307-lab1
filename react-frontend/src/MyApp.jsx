b193038515080bf87420be4ee0e1308b97031b30
RishanBaweja <rbaweja@calpoly.edu>
2025-10-06 20:58:07 -0700

    Backend talks to frontend!

--- react-frontend/src/MyApp.jsx
+++ react-frontend/src/MyApp.jsx
@@ -1,9 +1,7 @@
 // src/MyApp.jsx
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
-import Form from "./Form"
-import { useState } from "react"; 
-
+import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
@@ -11,24 +9,32 @@ function MyApp() {
     setCharacters([...characters, person]);
   }
 
-    function removeOneCharacter(index) {
-        const updated = characters.filter((character, i) => {
-            return i !== index;
-        });
-        setCharacters(updated);
-    }
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
-  <div className="container">
-    <Table
-      characterData={characters}
-      removeCharacter={removeOneCharacter}
-    />
-    <Form handleSubmit={updateList} />
-  </div>
-);
-
-
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+    </div>
+  );
 }
-
 export default MyApp;
